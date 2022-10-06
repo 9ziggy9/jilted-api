c1606d9d2a36a9bc40e3d8051b1d9d329bc0c4dd
9ziggy9 <davidarogers@protonmail.com>
2022-10-06 12:29:19 -0700

    styling and shtuff

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from ..models import db, Member
 
 bp = Blueprint("main", __name__, url_prefix="")
 
 @bp.route("/")
 def main():
-    return render_template("main.html", var="Test")
+    members = Member.query.all()
+    return render_template("main.html", members=members)
--- src/seeders/__init__.py
+++ src/seeders/__init__.py
@@ -1,36 +1,48 @@
 from ..models import db, Member
 from flask.cli import AppGroup
 
+def seed_members():
+    start_members = ["david", "brad", "john", "cesar", "ziggy",
+                     "matt", "jake", "gary", "amanda", "an", "sam",
+                     "ben", "brandon", "alex", "hai", "jack", "jae",
+                     "justin", "katy", "keanen", "kevin", "kyle",
+                     "logan", "lyn", "maliha", "fiona", "randy",
+                     "tony", "will", "yibo", "thenu", "nate"]
+
+    print("\n")
+    print("Seeding all members ...")
+
+    for m in start_members:
+        mem = Member(name=m)
+        db.session.add(mem)
+
+    db.session.commit()
+    
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_members():
+    db.session.execute('DELETE FROM members')
+    db.session.commit()
+
+# Create a seed group to hold our commands
 seed_commands = AppGroup("seed")
 
-def seed_members():
-    david = Member(name="david")
-    brad = Member(name="brad")
-    john = Member(name="john")
-    cesar = Member(name="cesar")
-    ziggy = Member(name="ziggy")
-
-    db.session.add(david)
-    db.session.add(brad)
-    db.session.add(john)
-    db.session.add(cesar)
-    db.session.add(ziggy)
-    db.session.commit()
-
-def undo_members():
-    db.session.execute("DELETE FROM members")
-    db.session.commit()
-
+# Defining commands
 @seed_commands.command("all")
 def seed():
     seed_members()
 
 @seed_commands.command("undo")
 def undo():
+    print("\n")
+    print("Clearing database")
+    print("\n")
     undo_members()
 
 @seed_commands.command("restore")
 def restore():
-    print("Purging database")
     db.drop_all()
     db.create_all()
